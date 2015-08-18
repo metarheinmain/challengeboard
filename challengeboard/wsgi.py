e7279fdeea39d5bf37fbbb38792080ba2f4156e5
Raphael Michel <mail@raphaelmichel.de>
2015-08-18 23:17:58 +0200

    Add only static files, not media files

--- challengeboard/wsgi.py
+++ challengeboard/wsgi.py
@@ -13,6 +13,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "challengeboard.settings")
 from django.core.wsgi import get_wsgi_application
 try:
     from dj_static import Cling, MediaCling
-    application = Cling(MediaCling(get_wsgi_application()))
+    application = Cling(get_wsgi_application())
 except ImportError:
     application = get_wsgi_application()
